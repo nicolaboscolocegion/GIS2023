0d1a7dbe9b881ba85f25c870e720897d270ae4ac
Nicola <nicolaboscolocegion@gmail.com>
2023-07-01 15:42:08 +0200

    fet: collect layers

--- app_src/openjmp_plug/plug.java
+++ app_src/openjmp_plug/plug.java
@@ -1,5 +1,50 @@
 package openjmp_plug;
 
-public class plug {
+import com.vividsolutions.jump.workbench.model.Layer;
+import com.vividsolutions.jump.workbench.plugin.AbstractPlugIn;
+import com.vividsolutions.jump.workbench.plugin.PlugInContext;
+import com.vividsolutions.jump.workbench.ui.plugin.FeatureInstaller;
 
+import java.util.List;
+
+import com.vividsolutions.jump.feature.BasicFeature;
+import com.vividsolutions.jump.feature.Feature;
+import com.vividsolutions.jump.feature.FeatureCollection;
+import com.vividsolutions.jump.feature.FeatureDataset;
+import com.vividsolutions.jump.feature.FeatureSchema;
+
+public class plug extends AbstractPlugIn{
+
+	@Override
+	public void initialize(PlugInContext context) throws Exception {
+		
+		//feature of the plug
+		FeatureInstaller fi = FeatureInstaller.getInstance(context.getWorkbenchContext()); // OpenJUMP >= 2.0
+		fi.addMainMenuPlugin(
+			this, //exe
+			new String[] {"GIS", "project" }, //menu path
+			this.getName(), //name .getName received by AbstractPlugIn
+			false, //Checkbooxs
+			null, //icon
+			null );
+	}
+	
+	@Override
+	public boolean execute(PlugInContext context) throws Exception {
+		
+		List<Layer> layers  = context.getLayerManager().getLayers();
+		
+		for(Layer l : layers) {
+			System.out.println(l.getName());
+		}
+		
+		 
+		return false;
+	}
+	
+	@Override
+	public String getName() {
+		//name of the plug
+		return "super plug";
+	}
 }
