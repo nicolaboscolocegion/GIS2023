f2d46d22f8af3d6d8a34f450a41bbadeec4729a0
Nicola <nicolaboscolocegion@gmail.com>
2023-07-05 09:38:39 +0200

    feat: find upstream and downstram stations

--- app_src/openjmp_plug/plug.java
+++ app_src/openjmp_plug/plug.java
@@ -1,6 +1,11 @@
 package openjmp_plug;
 
 import com.vividsolutions.jump.feature.Feature;
+import com.vividsolutions.jump.feature.FeatureCollection;
+import com.vividsolutions.jump.feature.FeatureDataset;
+import com.vividsolutions.jump.feature.FeatureSchema;
+
+import java.util.LinkedList;
 import com.vividsolutions.jump.workbench.model.Layer;
 
 
@@ -8,7 +13,9 @@ import com.vividsolutions.jump.workbench.plugin.AbstractPlugIn;
 import com.vividsolutions.jump.workbench.plugin.PlugInContext;
 import com.vividsolutions.jump.workbench.ui.plugin.FeatureInstaller;
 import com.vividsolutions.jump.workbench.ui.*;
-import javax.swing.JComboBox;
+
+import org.locationtech.jts.geom.Geometry;
+
 import java.util.List;
 
 public class plug extends AbstractPlugIn {
@@ -40,9 +47,7 @@ public class plug extends AbstractPlugIn {
 				context.getWorkbenchFrame(),
 				this.getName(), true );
 				String _namelayer = "Choose the layer to process";
-				mid.addLayerComboBox( _namelayer, null,
-				context.getLayerManager() );
-				mid.addAttributeComboBox("Report", _namelayer, new AttributeTypeFilter(AttributeTypeFilter.STRING), "");
+				mid.addLayerComboBox( _namelayer, null, context.getLayerManager() );
 				mid.setVisible( true ); // modal dialog
 				if( mid.wasOKPressed() == false ) return false;
 				
@@ -55,22 +60,102 @@ public class plug extends AbstractPlugIn {
 		
 		Layer inputLayer = context.getLayerManager().getLayer(name);
 		
+		List<String> reports = new LinkedList<String>();
+		
+		
 		for(Feature f : inputLayer.getFeatureCollectionWrapper().getFeatures()) {
-			System.out.println(f.getAttribute("altitude"));
+			reports.add(f.getString("name"));
+			System.out.println(f.getString("name"));
 		}
 		
 	
-		
+		//dialog for selecting report
 		MultiInputDialog mid1 = new MultiInputDialog(
 				context.getWorkbenchFrame(),
 				this.getName(), true );
 				String _nameReport = "Choose the layer to process";
-				
+				mid1.addComboBox(_nameReport, reports.get(0), reports, "yes");
 				mid1.setVisible( true ); // modal dialog
 				if( mid1.wasOKPressed() == false ) return false;
 		
+		String reportSelectedName = mid1.getText(_nameReport);
+		
+		Feature reportSelected=null;
+		
+		inputLayer.getFeatureCollectionWrapper().getFeatures().get(0).getSchema().getAttributeIndex("name");
+		for(Feature f : inputLayer.getFeatureCollectionWrapper().getFeatures()) {
+			
+			if(reportSelectedName.equals(f.getAttribute("name"))) {
+				reportSelected =f;
+			}	
+		}
+		
+		Geometry reportGeo = reportSelected.getGeometry();
+		
+		String[] stationLayers = {"idrografia-belluno-stazioni-fiumi-bio", "idrografia-belluno-stazioni-fiumi-chimico", "idrografia-belluno-stazioni-laghi-bio", "idrografia-belluno-stazioni-laghi-chimico"};
+		
+		Geometry[] upstreamStations = new Geometry[2];
+		Geometry downstramStation = null;
+		
+		double[] upstreamDistances = new double[2];
+		double downstramDistant = 0;
+		
+		int reportAltitude= 200;
+		
+		List<Feature> stations = new LinkedList();
+				
+		stations.addAll(context.getLayerManager().getLayer(stationLayers[0]).getFeatureCollectionWrapper().getFeatures());
+		stations.addAll(context.getLayerManager().getLayer(stationLayers[1]).getFeatureCollectionWrapper().getFeatures());
+		stations.addAll(context.getLayerManager().getLayer(stationLayers[2]).getFeatureCollectionWrapper().getFeatures());
+		stations.addAll(context.getLayerManager().getLayer(stationLayers[3]).getFeatureCollectionWrapper().getFeatures());
+
+		
+		//fid upstream and downstream nearest stations
+		for(Feature f : stations) {
+			System.out.println(f.getInteger(8));
+			int altitudeIndex = f.getSchema().getAttributeIndex("altitude");
+			double distanceTemp =0;
+			//if upstream
+			if(f.getInteger(altitudeIndex)>reportAltitude) {
+				if(upstreamDistances[0]!=0 && upstreamDistances[0] > (distanceTemp = reportGeo.distance(f.getGeometry()))) {
+					//swap the first with the second nearest
+					upstreamDistances[1]= upstreamDistances[0];
+					upstreamStations[1] =upstreamStations[0];
+					
+					upstreamDistances[0] =distanceTemp;
+					upstreamStations[0] = f.getGeometry();
+					continue;
+				}
+				
+				if(upstreamDistances[0]==0) {
+					upstreamDistances[0] =reportGeo.distance(f.getGeometry());
+					upstreamStations[0] = f.getGeometry();
+					continue;
+				}
+				
+			//if downstream
+			}else{
+				if(downstramDistant!=0 && downstramDistant > (distanceTemp = reportGeo.distance(f.getGeometry()))) {
+					//swap the first with the second nearest
+					
+					downstramDistant =distanceTemp;
+					downstramStation = f.getGeometry();
+					continue;
+				}
+				
+				if(downstramDistant==0) {
+					downstramDistant = reportGeo.distance(f.getGeometry());
+					downstramStation = f.getGeometry();
+					continue;
+				}
+			}
+		}
+		
+		FeatureSchema fs= new FeatureSchema();
 		
 		
+		
+		//FeatureCollection fc = new FeatureDataset();
 		System.out.println(name);
 		
 		return false;
