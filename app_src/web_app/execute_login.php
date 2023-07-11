9e61dcfeb4dbab107da542a1c426cbfedc119b8a
Mirco Cazzaro <mirco.cazzaro.1999@gmail.com>
2023-07-11 15:00:21 +0200

    added php files to git

---
+++ app_src/web_app/execute_login.php
@@ -0,0 +1,43 @@
+<?php
+
+    session_start();
+    
+    $servername = "localhost";
+    $username = "postgres";
+    $password = "***REMOVED***";
+    $dbname = "gis2023";
+
+
+    $conn = pg_connect("host=" . $servername . " port=5432 dbname=" . $dbname . " user=" . $username . " password=" . $password);
+    
+
+    
+    $sql = "SELECT id_user, users.user, pass, nome, cognome FROM users;";
+    $result = pg_query($conn, $sql);
+
+    $flag = 0;
+
+   
+        
+    while($row = pg_fetch_array($result)) {
+        if ($row['user'] == $_POST['user'] && $row['pass'] == hash("sha512", $_POST['pass'])) {
+            $_SESSION["id_utente"] = $row['id_utente'];
+            $_SESSION["user"] = $row["user"];
+            $_SESSION["nome"] = $row["nome"];
+            $_SESSION["cognome"] = $row["cognome"];
+            header("Location: visualizza_report.php");
+            $flag = 1;
+        }
+        else if ($row['user'] == $_POST['user'] && $row['pass'] != $_POST['pass']) {
+            $_POST['auth'] = 0;
+            require_once "login.php"; //PASSWORD ERRATA
+        }
+    }
+
+    if ($flag == 0) {
+        $_POST['auth'] = 1;
+        require_once "login.php"; //UTENTE NON TROVATO
+    }
+
+    
+?>
---
+++ app_src/web_app/js/all_visualizer.js
@@ -0,0 +1,96 @@
+var mapView = new ol.View ({
+    center: ol.proj.fromLonLat([12.123605, 46.472403]),
+    zoom:10
+});
+
+var map = new ol.Map ({
+    target: 'map',
+    view: mapView
+});
+
+var osmTile = new ol.layer.Tile ({
+    title: 'Open Street Map',
+    visible: true,
+    source: new ol.source.OSM()
+})
+
+map.addLayer(osmTile);
+
+var BellunoFiumi = new ol.layer.Tile({
+    title: "Fiumi di Belluno",
+    source: new ol.source.TileWMS({
+        url: 'http://gis2023.bitsei.it/geoserver/GIS_2023/wms',
+        params: {'LAYERS':'GIS_2023:bellunofiumi', 'TILED': true},
+        serverType: 'geoserver',
+        visible: true
+    })
+});
+
+var BellunoLaghi = new ol.layer.Tile({
+    title: "Laghi di Belluno",
+    source: new ol.source.TileWMS({
+        //url: 'http://89.40.142.15:8888/geoserver/GIS_2023/wms',
+        url: 'http://gis2023.bitsei.it/geoserver/GIS_2023/wms',
+        params: {'LAYERS':'GIS_2023:bellunolaghi', 'TILED': true},
+        serverType: 'geoserver',
+        visible: true
+    })
+});
+
+
+
+map.addLayer(BellunoLaghi)
+map.addLayer(BellunoFiumi)
+
+ 
+
+
+
+
+document.addEventListener("DOMContentLoaded",  function (event) {
+    latitudes = document.getElementById("lat").value;
+    longitudes = document.getElementById("long").value;
+
+    const arr1 = latitudes.split(",");
+    const arr2 = longitudes.split(",");
+
+    let i = 0;
+
+    while (i < arr1.length) {
+
+        var markerLayer = new ol.layer.Vector({
+            source: new ol.source.Vector(),
+        });
+        
+        map.addLayer(markerLayer);
+        
+        var markerFeature;
+        
+        var markerStyle = new ol.style.Style({
+        image: new ol.style.Icon({
+            src: 'location-pin.png', // Path to your custom marker icon
+            scale: 0.8, // Adjust the size of the marker icon as needed
+            offset: [0.5, 1], // Adjust the offset to align the marker correctly
+            anchor: [0.5, 1], // Set the anchor point to the bottom center of the marker icon
+        }),
+        });
+
+        var clickedCoordinate = [arr1[i], arr2[i]];
+
+        if (markerFeature) {
+            markerLayer.getSource().removeFeature(markerFeature);
+        }
+
+        markerFeature = new ol.Feature({
+            geometry: new ol.geom.Point(clickedCoordinate),
+        });
+
+        markerFeature.setStyle(markerStyle);
+
+        markerLayer.getSource().addFeature(markerFeature);
+
+        i++;
+    }
+
+    /**/
+});
