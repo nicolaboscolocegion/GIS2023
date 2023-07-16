704c58925bfd50d8730f4a1e72d70fa35d763f5d
Nicola <nicolaboscolocegion@gmail.com>
2023-07-16 19:12:11 +0200

    doc: log class

--- app_src/openjmp_plug/Login.java
+++ app_src/openjmp_plug/Login.java
@@ -4,52 +4,63 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+/**
+ * class for makking a login for the database
+ */
 public class Login extends JDialog implements ActionListener {
    JPanel panel;
-   JLabel user_label, password_label, message;
+   JLabel user_label, password_label;
    JTextField userName_text;
    JPasswordField password_text;
-   JButton submit, cancel;
+   JButton submit;
    private String userName;
    private char[] password;
    
+   /**
+    * creates a Jdialog for login
+    */
    Login() {
-      // Username Label
-	  setModal(true);
+      //Username Label
+      setModal(true);
       user_label = new JLabel();
       user_label.setText("User Name :");
       userName_text = new JTextField();
-      // Password Label
+      //Password Label
       password_label = new JLabel();
       password_label.setText("Password :");
       password_text = new JPasswordField();
-      // Submit
+      //Submit
       submit = new JButton("LOGIN");
       panel = new JPanel(new GridLayout(3, 1));
       panel.add(user_label);
       panel.add(userName_text);
       panel.add(password_label);
       panel.add(password_text);
-      message = new JLabel();
-      panel.add(message);
       panel.add(submit);
       
-      // Adding the listeners to components
+      //Adding the listeners to components
       submit.addActionListener(this);
       add(panel, BorderLayout.CENTER);
       setTitle("Database login");
       setSize(350,200);
       setVisible(true);
    }
-   
+
+   /**
+    * get the username inserd
+    * @return the username
+    */
    public String getUserName() {return userName;}
+
+   /**
+    * get the password
+    * @return the password
+    */
    public char[] getPassword() {return password;}
-   
-   public static void main(String[] args) {
-      Login l = new Login();
-      
-      System.out.println(l.getUserName() + new String(l.getPassword()));
-   }
+
+   /**
+    * save the username and password
+    */
    @Override
    public void actionPerformed(ActionEvent ae) {
       userName = userName_text.getText();
